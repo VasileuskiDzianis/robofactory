a7494f38919822e8e8c3c2090378acbdae524b63
Dzianis Vasileuski <d.vasileuski@gmail.com>
2017-10-27 11:44:36 +0300

    main web-page was added

--- src/main/java/by/vasilevsky/RobofactoryApplication.java
+++ src/main/java/by/vasilevsky/RobofactoryApplication.java
@@ -2,9 +2,10 @@ package by.vasilevsky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RobofactoryApplication {
+public class RobofactoryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RobofactoryApplication.class, args);
---
+++ src/main/java/by/vasilevsky/robofactory/web/MainPageController.java
@@ -0,0 +1,17 @@
+package by.vasilevsky.robofactory.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainPageController {
+	
+	
+	@RequestMapping("/")
+	public String mainPage(Model model) {
+		
+		return "main_page";
+	}
+
+}
